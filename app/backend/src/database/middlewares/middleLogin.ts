b344d4f7f5afd9f9039f73593a62e3c7cf2bc9ce
Victor Martins <victor.martins.duarte@gmail.com>
2022-07-21 01:53:38 -0300

    Feat: Res-07 Middleware for login validation that prevent access if password is not informed

--- app/backend/src/database/middlewares/middleLogin.ts
+++ app/backend/src/database/middlewares/middleLogin.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 const loginValid = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
   const emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
-  if (!email) return res.status(400).json({ message: 'All fields must be filled' });
+  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
   if (!emailRegex.test(email)) return res.status(401).json({ message: 'Invalid "email"' });
   if (password.length <= 6) {
     return res.status(401)
